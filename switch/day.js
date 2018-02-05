7baf78ed7f44026153fd68fea98dcc3e78e1f6cd
“BIT <classroom@bgit.rs>
2018-02-05 18:01:46 +0100

    zadaci arrays

---
+++ switch/day.js
@@ -0,0 +1,30 @@
+var day= 17;
+switch(day){
+    case 1:
+
+          console.log("Monday");
+          break;
+ case 2:
+          console.log("Thuesday");
+            break;
+ case 3:
+         console.log("Weansday");
+         break;
+case 4: 
+        console.log("Thursday");
+        break;
+
+case 5:
+        console.log("friday");
+        break;
+case 6:
+       console.log("Saturday");
+       break;
+
+
+case 7:
+    console.log("Sunday");
+    break
+    default:
+    console.log( "Input must be a number between 1 and 7")
+}
---
+++ switch/months.js
@@ -0,0 +1,15 @@
+/*var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+months;
+console.log(months[5], months[9],months[0]);
+*/
+ var days = ["Monday", "Tuesday", "Weansday", "Thursday", "Friday", "Saturday", "Sunday"];
+ days;
+ console.log(days[6]);
+
+
+ var number=[2,-4,5,-2,-11]
+ number; console.log(number[1], number[3], number [4]);
+
+ var divisible = [5,15,-5,20,12,18,72,14,9];
+ divisible;
+ console.log(divisible[1],divisible[4],divisible[5],divisible[8]);
