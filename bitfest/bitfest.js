45283c4ff7aa8a742192a9700bf82b6f85132993
BIT Student <classroom@bit.rs>
2018-04-04 09:33:20 +0200

    BitFest views

---
+++ bitfest/bitfest.js
@@ -0,0 +1,8 @@
+var movieTitle = document.querySelector("#title").value;
+var movieLength = document.querySelector('#number').value;
+var movieGenre = document.querySelector("#genre").value;
+var addMovieButton = document.querySelector('#addprogram');
+
+addMovieButton.addEventListener('click', function (event) {
+    alert('Element clicked through function!')
+});
